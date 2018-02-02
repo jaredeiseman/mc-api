8a8542a302d072c6e028bbabe05b155b583368f9
Jared Eiseman <jathei@gmail.com>
2018-02-02 15:22:37 -0800

    change database to postgres

--- mc/settings.py
+++ mc/settings.py
@@ -98,10 +98,20 @@ WSGI_APPLICATION = 'mc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mamas-connect',
+        'USER': 'mcadmin',
+        'PASSWORD': 'mcadminpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
